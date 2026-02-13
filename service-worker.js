85abba91b5f94444daa81e13a3cd20885c394f2e
monkeydluffyzoro0100-jpg <monkey.d.luffy.zoro0100@gmail.com>
2026-02-13 21:57:47 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('zoro-diary').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
